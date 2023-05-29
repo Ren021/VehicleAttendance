f1208b5d67145f732568c2bd2af98dcc25a73ffa
Ren021 <132036831+Ren021@users.noreply.github.com>
2023-05-29 21:27:23 +0800

    Add files via upload

---
+++ BoatClass.java
@@ -0,0 +1,17 @@
+public class BoatClass extends VehicleClass{
+	
+	
+	@Override
+	void MainSailColor() {
+		
+		System.out.println("MainSail Color: Black");
+	}
+	void brand( ) {
+		
+		System.out.println("Fandango Yacht");
+	}
+	void drive() {
+		System.out.println("Fandango Yacht was launched on the water");
+	}
+
+}
---
+++ VehicleClass.java
@@ -0,0 +1,16 @@
+public class VehicleClass {
+	
+	String price;
+	String color;
+	String speed;
+	
+	void stop() {
+		
+		System.out.println("Stop!");
+	}
+	void MainSailColor() {
+		
+		System.out.println("MainSail Color: Not Type");
+	}
+
+}
